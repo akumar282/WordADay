d2044de6d5d2cdc3d3fb03119946267ef3b1a870
akumar282 <abhi.kumar7567@gmail.com>
2022-09-10 21:05:36 -0700

    update 1.0.2

--- src/main.ts
+++ src/main.ts
@@ -11,22 +11,43 @@ const userClient = new TwitterApi({
   accessSecret: apiconfig.accessSecret
 })
 // const rwClient = userClient.readWrite
-function delay(ms: number) {
+function delay (ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
-async function queryWord (word: string): Promise<string> {
+
+async function queryWord (word: string| undefined): Promise<string | undefined> {
   const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
   const data = await response.json()
-  console.log(data[0].meanings[0].definitions[0].definition)
+  if (!data[0].meanings[0]) {
+    return undefined
+  }
   return data[0].meanings[0].definitions[0].definition
 }
-while (true) {
-  let word
-  do {
-    word = wl.getWord()
-  } while (!word)
 
-  const def = await queryWord(word)
-  userClient.v1.tweet('The Word Of The Day Is: ' + word + '\n' + 'The Definition is: ' + def)
+async function chooseWord (): Promise<(string | undefined)[]> {
+  const ret: (string | undefined)[] = []
+
+  const word = wl.getWord()
+
+  const definition = await queryWord(word)
+  ret.push(word, definition)
+
+  return ret
+}
+
+while (true) {
+  let isItNull : boolean
+  let data:(string|undefined)[]
+  do {
+    isItNull = false
+    data = await chooseWord()
+    for (const w of data) {
+      if (!w) {
+        isItNull = true
+      }
+    }
+  } while (isItNull)
+
+  userClient.v1.tweet('The Word Of The Day Is: ' + data[0] + '\n' + 'The Definition is: ' + data[1])
   await delay(24 * 60 * 60 * 1000)
 }
